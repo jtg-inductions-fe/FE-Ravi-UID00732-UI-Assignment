430bafe106d1368dded33ea7749be620aa71da0e
Ravi <ravi@joshtechnologygroup.com>
2026-07-08 15:17:38 +0530

    [RA_A1_02]: dummy commit

--- src/script/index.js
+++ src/script/index.js
@@ -1,5 +1,5 @@
-const hamburger = document.getElementById('hamburger');
-const navMenu = document.getElementById('nav-menu');
+const hamburger = document.getElementsByClassName('hamburger__container')[0];
+const navMenu = document.getElementsByClassName('navbar__menu')[0];
 
 hamburger.addEventListener('click', () => {
     navMenu.classList.toggle('is-open');
