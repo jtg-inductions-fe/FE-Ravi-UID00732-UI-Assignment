ba4419835be6d6bd2926bd382b00cb8941ae3da5
Ravi <ravi@joshtechnologygroup.com>
2026-07-09 12:49:59 +0530

    [RA_A1_01]: Added hamburger background color

--- src/script/index.js
+++ src/script/index.js
@@ -1,6 +1,14 @@
 const hamburger = document.getElementById('hamburger');
+const hamburgerIcon = document.getElementById('hamburger-icon');
 const navMenu = document.getElementById('nav-menu');
 
 hamburger.addEventListener('click', () => {
     navMenu.classList.toggle('is-open');
+    hamburger.classList.toggle('is-active');
+
+    if (hamburger.classList.contains('is-active')) {
+        hamburgerIcon.src = 'public/assets/images/hamburger_white.svg';
+    } else {
+        hamburgerIcon.src = 'public/assets/images/hamburger_blue.svg';
+    }
 });
