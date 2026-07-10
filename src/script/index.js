28d3b82e6e399f908c91a6ddb766088da7828b9c
Ravi <ravi@joshtechnologygroup.com>
2026-07-10 07:57:16 +0530

    [RA_A1_03]: latest-release section changes

--- src/script/index.js
+++ src/script/index.js
@@ -1,6 +1,30 @@
+import Swiper from 'swiper';
+import { Pagination } from 'swiper/modules';
+
 const hamburger = document.getElementsByClassName('hamburger__container')[0];
 const navMenu = document.getElementsByClassName('navbar__menu')[0];
 
 hamburger.addEventListener('click', () => {
     navMenu.classList.toggle('is-open');
+});
+
+new Swiper('#releases-slider', {
+    modules: [Pagination],
+
+    loop: false,
+    centeredSlides: true,
+    slidesPerView: 2,
+    spaceBetween: 16,
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+
+    breakpoints: {
+        603: {
+            slidesPerView: 3,
+            spaceBetween: 24,
+        },
+    },
 });
